4e7424944362e841324b0399599554913bea5a5f
9insane9 <145199591+9insane9@users.noreply.github.com>
2026-07-21 18:46:52 +0300

    add basic tests

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,70 +1,70 @@
-const db = require("../db/queries/queries")
-const { validateCategory, validateItem } = require("../validators")
+const db = require("../db/queries/queries");
+// const { validateCategory, validateItem } = require("../validators")
 
 async function getRenderItemList(req, res) {
-  let categoryId
+  let categoryId;
 
   if (req.params.id === "uncategorized") {
-    categoryId = null
+    categoryId = null;
   } else if (req.params.id) {
-    categoryId = Number(req.params.id)
+    categoryId = Number(req.params.id);
   } else {
-    categoryId = undefined
+    categoryId = undefined;
   }
 
-  const itemRows = await db.getItemsByCategory(categoryId)
-  const categoryRows = await db.getCategories()
+  const itemRows = await db.getItemsByCategory(categoryId);
+  const categoryRows = await db.getCategories();
 
-  res.render("index", { itemRows, categoryRows, categoryId, errors: null })
+  res.render("index", { itemRows, categoryRows, categoryId, errors: null });
 }
 
 async function getRenderCategoryEditor(req, res) {
-  const categoryRows = await db.getCategories()
+  const categoryRows = await db.getCategories();
 
-  res.render("categoryEdit", { categoryRows, errors: null })
+  res.render("categoryEdit", { categoryRows, errors: null });
 }
 
 async function addCategory(req, res) {
   try {
-    const errors = req.errors
+    const errors = req.errors;
     if (errors) {
-      return res.status(400).json({ errors })
+      return res.status(400).json({ errors });
     }
 
-    const { name, icon_id } = req.body
-    await db.addCategory(name, icon_id)
-    res.status(201).json({ message: "Category added successfully" })
+    const { name, icon_id } = req.body;
+    await db.addCategory(name, icon_id);
+    res.status(201).json({ message: "Category added successfully" });
   } catch (err) {
-    console.error("Error adding category:", err)
-    res.status(500).json({ error: "Error adding category" })
+    console.error("Error adding category:", err);
+    res.status(500).json({ error: "Error adding category" });
   }
 }
 
 async function updateCategory(req, res) {
   try {
-    const errors = req.errors
+    const errors = req.errors;
     if (errors) {
-      return res.status(400).json({ errors })
+      return res.status(400).json({ errors });
     }
 
-    const { name, icon_id } = req.body
-    const { id } = req.params
-    await db.updateCategory(id, name, icon_id)
-    res.json({ message: "Category updated successfully" })
+    const { name, icon_id } = req.body;
+    const { id } = req.params;
+    await db.updateCategory(id, name, icon_id);
+    res.json({ message: "Category updated successfully" });
   } catch (err) {
-    console.error("Error updating category:", err)
-    res.status(500).json({ error: "Error updating category" })
+    console.error("Error updating category:", err);
+    res.status(500).json({ error: "Error updating category" });
   }
 }
 
 async function deleteCategory(req, res) {
   try {
-    const { id } = req.params
-    await db.deleteCategory(id)
-    res.sendStatus(200)
+    const { id } = req.params;
+    await db.deleteCategory(id);
+    res.sendStatus(200);
   } catch (err) {
-    console.error("Error deleting category:", err)
-    res.status(500).send("Error deleting category")
+    console.error("Error deleting category:", err);
+    res.status(500).send("Error deleting category");
   }
 }
 
@@ -74,4 +74,4 @@ module.exports = {
   addCategory,
   updateCategory,
   deleteCategory,
-}
+};
--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,8 @@
-const { Pool } = require("pg")
+const { Pool } = require("pg");
 
-const localConnectionString = process.env.LOCAL_CONNECTION_STRING
-const prodConnectionString = process.env.NEON_CONNECTION_STRING
+// const localConnectionString = process.env.LOCAL_CONNECTION_STRING
+const prodConnectionString = process.env.NEON_CONNECTION_STRING;
 
 module.exports = new Pool({
   connectionString: prodConnectionString,
-})
+});
